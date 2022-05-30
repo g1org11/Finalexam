f9fd9cbf6e0033339f4c283160602e2102749a4e
GIORGI <khoshtaria.gio@gmail.com>
2022-05-30 13:21:37 +0400

    add contact

--- main.js
+++ main.js
@@ -65,3 +65,48 @@ function handleBallClick(nextIndex) {
 }
 /////////////
 const work = document.querySelector(".work");
+/////////////
+//last projects
+filter("all");
+function filter(c) {
+  let x, i;
+  x = document.getElementsByClassName("project-contents");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+  }
+}
+
+function AddClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function RemoveClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+let btnContainer = document.getElementById("sidebar");
+let btns = document.getElementsByClassName("btn");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
